9d11caf1d162da6c49cb7ccb08cf4ea10d31aba4
Aspen <thecrimsonmecha@gmail.com>
2022-03-30 20:53:06 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
-const e = require('express');
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -9,27 +7,7 @@ const port = process.env.PORT || 8080;
 app.engine('html', require('ejs').renderFile);
 
 app.get('/', (req, res) => {
-    
-    var count;
-    
-    fs.readFile('count.txt', 'utf8', function (err, data) {
-        if (err) {
-          return console.log(err);
-        }
-
-        count = parseInt(data);
-        count++;
-        count = count.toString();
-
-        fs.writeFile('count.txt', count, err => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-        });
-    });
-
-    res.render('index.html', {count: count});
+    res.render('index.html');
 })
 
 app.listen(port, () => {
